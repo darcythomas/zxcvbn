e0dbc0a2db05fe693ad437bd1d86db2d303ca25f
darcythomas <github@darcy.io>
2014-01-16 21:34:02 +1300

    Basics kinda working

---
+++ zxcvbn.net/zxcvbnTests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using zxcvbn.net;
+
+namespace zxcvbnTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        String lamePassword = "Passw0rd1!";
+
+        [TestMethod]
+        public void HelloCode()
+        {
+            Assert.IsTrue(true,"Hello world unit test");
+        }
+
+
+        [TestMethod]
+        public void CanSetAndGetGlobalVariable()
+        {
+            String lameString = "Passw0rd1!";
+            Zxcvbn zxcvbn = new Zxcvbn();
+
+            int zero = 0;
+            dynamic actual = zxcvbn.Test(lamePassword);
+            Assert.AreEqual(lameString, actual.ToString(), "Hello world unit test");
+        }
+
+        [TestMethod]
+        public void ScoreIsZeroWithLamePassword()
+        {
+            String lamePassword = "Passw0rd1!";
+           Zxcvbn zxcvbn = new Zxcvbn();
+
+            int zero = 0;
+            int expected = zxcvbn.Score(lamePassword);
+            Assert.AreEqual(zero, expected, "Hello world unit test");
+        }
+
+    }
+}
