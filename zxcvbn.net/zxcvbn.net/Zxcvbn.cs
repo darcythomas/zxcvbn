966a4d09194022fd7858a0d3b0cb747d91bb3695
darcythomas <github@darcy.io>
2014-01-19 11:57:27 +1300

    Further added static class and extra functionality

--- zxcvbn.net/zxcvbn.net/Zxcvbn.cs
+++ zxcvbn.net/zxcvbn.net/Zxcvbn.cs
@@ -1,76 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Jurassic;
 
 namespace zxcvbn.net
 {
-    public class Zxcvbn
+    public static class Zxcvbn 
     {
+        private static ZxcvbnInstance Instance { get { return LazyLoadZxcvbnInstance.Value; } }
+        private static readonly Lazy<ZxcvbnInstance> LazyLoadZxcvbnInstance = new Lazy<ZxcvbnInstance>(() => new ZxcvbnInstance());
 
-        public static ScriptEngine Engine { get { return LazyLoadEngine.Value; } }
-
-        private static readonly Lazy<ScriptEngine> LazyLoadEngine = new Lazy<ScriptEngine>(ScriptEngine);
-
-        private static ScriptEngine ScriptEngine()
+        public static int Score(String password)
         {
-            String path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\js\";
-            ScriptEngine engine = new ScriptEngine();
-            engine.ExecuteFile(path + "zxcvbn.js");
-            return engine;
+            return Instance.Score(password);
         }
 
-      
-
-
-
-        public int Score(String password)
+        public static Double Entropy(String password)
         {
-            dynamic result = Test(password);
-            return result.score;
+            return Instance.Entropy(password);
         }
 
-        //public float entropy(String password)
-        //{
-
-        //}
-        //result.crack_time         # estimation of actual crack time, in seconds.
-
-        //result.crack_time_display # same crack time, as a friendlier string:
-        //                          # "instant", "6 minutes", "centuries", etc.
-
-        //result.score              # [0,1,2,3,4] if crack time is less than
-        //                          # [10**2, 10**4, 10**6, 10**8, Infinity].
-        //                          # (useful for implementing a strength bar.)
-
-        //result.match_sequence     # the list of patterns that zxcvbn based the
-        //                          # entropy calculation on.
-
-        //result.calculation_time   # how long it took to calculate an answer,
-        //                          # in milliseconds. usually only a few ms.
-
-        public object Test(string password)
+        public static int crack_time(String password)
         {
-
-            var stopwatch = Stopwatch.StartNew();
-            stopwatch.Start();
-         
-
-
-            Engine.SetGlobalValue("password", password);
-
-            var q = Engine.GetGlobalValue<string>("password");
-
-
-            stopwatch.Stop();
-            return q;
-
+            return Instance.crack_time(password);
         }
 
+        public static String crack_time_display(String password)
+        {
+            return Instance.crack_time_display(password);
+        }
+
+        public static List<String> match_sequence(String password)
+        {
+            return Instance.match_sequence(password);
+        }
+
+        public static int calculation_time(String password)
+        {
+            return Instance.calculation_time(password);
+        }
     }
 }
